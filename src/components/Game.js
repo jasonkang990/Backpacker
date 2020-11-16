8c083891ab71a7532fa6a77235f96ebacfba62ef
Lucas Chen <lucaschen1000@gmail.com>
2020-11-15 23:30:24 -0500

    Fragment is above backpack

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./Game.css";
 import Board from "./Board";
+import { noSquares } from "../config";
 
 export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{squares: Array(25).fill(null)}],
-      stepNumber: 0,
-      xIsNext: true
+      history: [{squares: Array(noSquares).fill(null)}],
+      stepNumber: 0
     };
   }
 
