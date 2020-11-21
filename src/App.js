74c52efb26f316ecbe5a2580213fe1641652a517
Lucas Chen <lucaschen1000@gmail.com>
2020-11-21 17:05:21 -0500

    Fix merge conflict

--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ import {
 } from "react-router-dom";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board:Array(36).fill('w')
-    };
-    this.handleBoard = this.handleBoard.bind(this);
-  }
-  
-  handleBoard(b, c) {
-    let newBoard = this.state.board;
-    newBoard[b] = c;
-    this.setState({board:newBoard});
-  }
+
 
   render() {
     
--- src/Backpacker.js
+++ src/Backpacker.js
@@ -5,6 +5,20 @@ import Game from './components/Game';
 import Ship from './components/Ship';
 
 export default class Backpacker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board:Array(36).fill('w')
+    };
+    this.handleBoard = this.handleBoard.bind(this);
+  }
+  
+  handleBoard(b, c) {
+    let newBoard = this.state.board;
+    newBoard[b] = c;
+    this.setState({board:newBoard});
+  }
+
   render() {
     return (
       <div className = "outer container">
