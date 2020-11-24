74f9746461162895488b4618cdb5575e60d04163
Lucas Chen <lucaschen1000@gmail.com>
2020-11-24 00:13:20 -0500

    add tabs

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -6,11 +6,11 @@ export default class Tutorial extends React.Component {
   render() {
     return (
       <div className="tutorial">
-        To play the game: <br/>
-        1) Drag items from the left side to the grid <br/>
-        2) Fit as many items as you can <br/>
-        3) When you can't fit anymore items, press ship it <br/>
-        4) Score is across lifetime <br/> 
+        &emsp;&emsp;To play the game: <br/>
+        &emsp;&emsp;1) Drag items from the left side to the grid <br/>
+        &emsp;&emsp;2) Fit as many items as you can <br/>
+        &emsp;&emsp;3) When you can't fit anymore items, press ship it <br/>
+        &emsp;&emsp;4) Score is across lifetime <br/> 
       </div>
     );
   }
